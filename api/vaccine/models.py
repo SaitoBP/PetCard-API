e1a5ba6c2039682151afe58ff562c804da03595d
Gabriel Saito <gabrielpereira050201@hotmail.com>
2020-10-18 17:10:41 -0300

    Added __str__ methods

--- api/vaccine/models.py
+++ api/vaccine/models.py
@@ -5,6 +5,9 @@ class Vaccine(models.Model):
     vaccine = models.CharField(max_length=255)
     description = models.TextField()
 
+    def __str__(self):
+        return f'{self.vaccine}'
+
 
 class VaccineSchedule(models.Model):
     CYCLE_CHOICES = [
@@ -18,3 +21,6 @@ class VaccineSchedule(models.Model):
     schedule = models.DateTimeField()
     status = models.BooleanField(default=False)
     cycle = models.CharField(max_length=1, choices=CYCLE_CHOICES, default='D')
+
+    def __str__(self):
+        return f'{self.vaccine.vaccine} - {self.schedule}'
