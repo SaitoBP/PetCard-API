8d6046afcc050c89c444b5a7f89012afea4410da
Gabriel Saito <gabrielpereira050201@hotmail.com>
2020-10-18 16:56:31 -0300

    Created vaccine models

--- api/vaccine/models.py
+++ api/vaccine/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Vaccine(models.Model):
+    vaccine = models.CharField(max_length=255)
+    description = models.TextField()
+
+
+class VaccineSchedule(models.Model):
+    CYCLE_CHOICES = [
+        ('D', 'DAILY'),
+        ('W', 'WEEKLY'),
+        ('M', 'MONTHLY'),
+        ('Y', 'YEARLY')
+    ]
+
+    vaccine = models.ForeignKey(Vaccine, related_name='vaccine_schedule', on_delete=models.CASCADE)
+    schedule = models.DateTimeField()
+    status = models.BooleanField(default=False)
+    cycle = models.CharField(max_length=1, choices=CYCLE_CHOICES, default='D')
