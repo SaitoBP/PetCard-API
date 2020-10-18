40d8e4b3d6f91ff6a1b6f19c647e10350f28ff79
Gabriel Saito <gabrielpereira050201@hotmail.com>
2020-10-18 16:43:58 -0300

    Added medication attribute to MedicationSchedule Model

--- api/medication/models.py
+++ api/medication/models.py
@@ -10,18 +10,18 @@ class MedicationSchedule(models.Model):
 
     ]
 
+    medication = models.ForeignKey('Medication', related_name='medication_schedule', on_delete=models.CASCADE)
     schedule = models.DateTimeField()
     cycle = models.CharField(max_length=1, choices=CYCLE_CHOICES)
     medication_status = models.BooleanField(default=False)
 
     def __str__(self):
-        return f"{self.schedule} - {self.cycle} - [{self.medication_status}]"
+        return f"{self.medication} - {self.schedule}"
 
 
 class Medication(models.Model):
     medicine = models.CharField(max_length=255)
     description = models.TextField()
-    medication_schedule = models.ForeignKey(MedicationSchedule, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.medicine
