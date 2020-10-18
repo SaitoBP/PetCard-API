5e022d232a16040c04431701eee8c12a69fba561
Gabriel Saito <gabrielpereira050201@hotmail.com>
2020-10-18 10:23:05 -0300

    Changed attribute name and added __str__ methods

--- api/medication/models.py
+++ api/medication/models.py
@@ -10,11 +10,18 @@ class MedicationSchedule(models.Model):
 
     ]
 
-    medication_schedule = models.DateTimeField()
+    schedule = models.DateTimeField()
     cycle = models.CharField(max_length=1, choices=CYCLE_CHOICES)
     medication_status = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.schedule} - {self.cycle} - [{self.medication_status}]"
 
 
 class Medication(models.Model):
     medicine = models.CharField(max_length=255)
+    description = models.TextField()
     medication_schedule = models.ForeignKey(MedicationSchedule, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.medicine
