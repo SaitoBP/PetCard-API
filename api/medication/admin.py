9af0708c00e1d5832e94bd8bb70a31b1121ed789
Gabriel Saito <gabrielpereira050201@hotmail.com>
2020-10-18 08:20:16 -0300

    Registered Medication App in admin panel

--- api/medication/admin.py
+++ api/medication/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from api.medication.models import Medication, MedicationSchedule
+
+admin.site.register(Medication)
+admin.site.register(MedicationSchedule)
