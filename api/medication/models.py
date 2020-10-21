758255e33e2cfa79f2851c39a7e18e67282ceefb
Gabriel Saito <gabrielpereira050201@hotmail.com>
2020-10-20 22:50:56 -0300

    Added business logic

--- api/medication/models.py
+++ api/medication/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from dateutil.relativedelta import relativedelta
+
 
 class MedicationSchedule(models.Model):
     CYCLE_CHOICES = [
@@ -13,7 +15,39 @@ class MedicationSchedule(models.Model):
     medication = models.ForeignKey('Medication', related_name='medication_schedule', on_delete=models.CASCADE)
     schedule = models.DateTimeField()
     cycle = models.CharField(max_length=1, choices=CYCLE_CHOICES)
-    medication_status = models.BooleanField(default=False)
+    status = models.BooleanField(default=False)
+
+    @property
+    def validate_status(self):
+        next_schedule = None
+
+        if self.cycle == 'D':
+            next_schedule = self.schedule + relativedelta(days=+1)
+
+        elif self.cycle == 'W':
+            next_schedule = self.schedule + relativedelta(weeks=+1)
+
+        elif self.cycle == 'M':
+            next_schedule = self.schedule + relativedelta(months=+1)
+
+        else:
+            next_schedule = self.schedule + relativedelta(years=+1)
+
+            """
+            Debug example
+            """
+            # schedule = 20
+            # next_schedule = schedule + 1
+
+            # if schedule >= next_schedule:
+            #     self.status = False
+            #     self.save()
+
+        if self.schedule >= next_schedule:
+            self.status = False
+            self.save()
+
+        return self.status
 
     def __str__(self):
         return f"{self.medication} - {self.schedule}"
