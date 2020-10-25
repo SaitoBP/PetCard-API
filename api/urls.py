605c6daaef11f93f75051ba15e57f4e7df51abfb
Gabriel Saito <gabrielpereira050201@hotmail.com>
2020-10-25 09:45:45 -0300

    Added a redirect url in urlpatterns

--- api/urls.py
+++ api/urls.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import RedirectView
 
 from rest_framework import routers
 
@@ -34,6 +35,7 @@ router.register('vaccine', VaccineViewSet, basename='Vaccine')
 router.register('vaccine_schedule', VaccineScheduleViewSet, basename='Vaccine Schedule')
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/api/')),
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
